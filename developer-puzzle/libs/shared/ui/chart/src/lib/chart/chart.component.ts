d2bd212e332fda862f84581c7d9fc32df4fb17da
Chadha, Abhijit <Abhijit.Chadha3@T-Mobile.com>
2020-01-21 01:32:49 -0800

    Task 2 changes

--- developer-puzzle/libs/shared/ui/chart/src/lib/chart/chart.component.ts
+++ developer-puzzle/libs/shared/ui/chart/src/lib/chart/chart.component.ts
@@ -3,8 +3,7 @@ import {
   ChangeDetectorRef,
   Component,
   Input,
-  OnInit,
-  OnDestroy
+  OnInit
 } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -13,14 +12,13 @@ import { Observable } from 'rxjs';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit, OnDestroy {
+export class ChartComponent implements OnInit {
   @Input() data$: Observable<any>;
   chartData: any;
 
   chart: {
     title: string;
     type: string;
-    data: any;
     columnNames: string[];
     options: any;
   };
@@ -30,14 +28,8 @@ export class ChartComponent implements OnInit, OnDestroy {
     this.chart = {
       title: '',
       type: 'LineChart',
-      data: [],
       columnNames: ['period', 'close'],
       options: { title: `Stock price`, width: '600', height: '400' }
     };
-    this.data$.subscribe(newData => (this.chartData = newData));
   }
-  ngOnDestroy() {
-    this.chartData.unsubscribe();
-  }
-
 }
